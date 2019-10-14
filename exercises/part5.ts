e357b38bc17172702fa31810f3dceecfc1230242
Carolista <55961845+Carolista@users.noreply.github.com>
2019-10-14 13:00:05 -0500

    part 5 done

--- exercises/part5.ts
+++ exercises/part5.ts
@@ -1,27 +1,33 @@
 // Add your import statement here:
-
+import { SpaceLocation } from './SpaceLocation';
 
 let kilometersToMars: number = 225000000;
 let kilometersToTheMoon: number = 384400;
 
 class Spacecraft {
-  milesPerKilometer: number = 0.621;
-  name: string;
-  speedMph: number;
+    milesPerKilometer: number = 0.621;
+    name: string;
+    speedMph: number;
 
-  constructor(name: string, speedMph: number) {
-    this.name = name;
-    this.speedMph = speedMph;
-  }
+    constructor(name: string, speedMph: number) {
+        this.name = name;
+        this.speedMph = speedMph;
+    }
 
-  getDaysToLocation(kilometersAway: number): number {
-    let milesAway: number = kilometersAway * this.milesPerKilometer;
-    let hours: number = milesAway / this.speedMph;
-    return hours / 24;
-  }
+    getDaysToLocation(kilometersAway: number): number {
+        let milesAway: number = kilometersAway * this.milesPerKilometer;
+        let hours: number = milesAway / this.speedMph;
+        return hours / 24;
+    }
 
-  // Add the printDaysToLocation function here.
-
+    // Add the printDaysToLocation function here.
+    printDaysToLocation(location: SpaceLocation) {
+        console.log(`It will take the shuttle ${this.name} ${this.getDaysToLocation(location.kilometersAway)} days to get to ${location.name}.`);
+    }
 }
 
 // Paste in the code from step 6 here:
+
+let spaceShuttle = new Spacecraft('Determination', 17500);
+spaceShuttle.printDaysToLocation(new SpaceLocation('Mars', kilometersToMars));
+spaceShuttle.printDaysToLocation(new SpaceLocation('the Moon', kilometersToTheMoon));
